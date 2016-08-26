cb519c905907cb6bcedef00dc0246c8d341a821e
Jake Worth <jakeworth82@gmail.com>
2016-08-25 21:08:04 -0500

    Add throttling to staging

--- config.ru
+++ config.ru
@@ -1,4 +1,7 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment', __FILE__)
+
+
+use Rack::Throttle::Interval, min: 0.5, :key_prefix => :throttle
 run Rails.application
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,1 +1,15 @@
 load Rails.root.join('config/environments/production.rb')
+require 'dalli'
+require 'rack/throttle'
+
+Rails.application.configure do
+  config.cache_store = :dalli_store,
+                      (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                      {:username => ENV["MEMCACHIER_USERNAME"],
+                       :password => ENV["MEMCACHIER_PASSWORD"],
+                       :failover => true,
+                       :socket_timeout => 1.5,
+                       :socket_failure_delay => 0.2
+                      }
+
+end
